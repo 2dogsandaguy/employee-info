c21b1ff7a09e0cf0236e1134f0029240773c8abf
2dogsandaguy <Neverclear@ymail.com>
2023-09-20 15:33:07 -0400

    add userPrompt.js to project with prompts

--- js/connection.js
+++ js/connection.js
@@ -4,21 +4,21 @@ const dotenv = require('dotenv');
 // Load environment variables from .env file
 dotenv.config();
 
-const connection = mysql.createConnection({
+const storageData = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE
 });
 
-connection.connect((err) => {
+storageData.connect((err) => {
   if (err) {
-    console.error('Error connecting to the database:', err);
+    console.error('Error connecting to the Data Base:', err);
     throw err;
   }
-  console.log('Connected to the database.');
+  console.log('Connected to the Data Base.');
 });
 
-module.exports = connection;
+module.exports = storageData;
 
 
--- server.js
+++ server.js
@@ -1,2 +1,23 @@
 const inquirer = require('inquirer');
-const connection = require('./js/connection');
+const storageData = require('./js/connection');
+const userPrompt = require('./js/userPrompt');
+
+userPrompt()
+
+
+storageData.connect((err => {
+  if (err) {
+    console.error('Error connection to the Data Base:', err);
+  }else {
+    afterConnection();
+  }
+}));
+
+const afterConnection = () => {
+  console.log('***********************************');
+  console.log('*                                 *');
+  console.log('*        EMPLOYEE MANAGER         *');
+  console.log('*                                 *');
+  console.log('***********************************');
+  promptUser();
+};
